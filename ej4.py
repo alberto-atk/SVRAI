899c9c5504965038315d4b22052711104c80800f
alberto-atk <73551392+alberto-atk@users.noreply.github.com>
2023-02-22 12:00:04 +0100

    se desplaza bien en todas direcciones

--- ej4.py
+++ ej4.py
@@ -14,9 +14,11 @@ class Entorno:
             for j in range(COLUMNAS):
                 self.cuadricula[i].append(0)
 
+        self.cuadricula[0][0] = 4
+        self.cuadricula[2][0] = 2
         self.acciones = acciones
-        self._anyadirNumero()
-        self._anyadirNumero()
+        #self._anyadirNumero()
+        #self._anyadirNumero()
         #self.estado_actual[0] = random.randint(0, FILAS)
         #self.estado_actual[1] = random.randint(0, COLUMNAS)
 
@@ -32,10 +34,73 @@ class Entorno:
 
         self.cuadricula[casilla[0]][casilla[1]] = random.choice([2,4])
 
+
+    def comprimir_casillas(self,i1,j1,i2,j2,i3,j3):
+        if self.cuadricula[i1][j1] == self.cuadricula[i2][j2]:
+            self.cuadricula[i3][j3] = self.cuadricula[i1][j1] + self.cuadricula[i2][j2]
+        
+
+    def desplazar_casillas_right(self,i,j):
+        aux1 = j
+        aux2 = j +1
+        while aux2 < COLUMNAS and self.cuadricula[i][aux2] == 0:
+            self.cuadricula[i][aux2] = self.cuadricula[i][aux1]
+            self.cuadricula[i][aux1] = 0
+            aux1 += 1
+            aux2 += 1  
+
+    def desplazar_casillas_left(self,i,j):        
+        aux1 = j
+        aux2 = j -1
+        while aux2 >= 0 and self.cuadricula[i][aux2] == 0:
+            self.cuadricula[i][aux2] = self.cuadricula[i][aux1]
+            self.cuadricula[i][aux1] = 0
+            aux1 -= 1
+            aux2 -= 1  
+
+    def desplazar_casillas_up(self,i,j):        
+
+        aux1 = i
+        aux2 = i -1
+        while aux2 >= 0 and self.cuadricula[aux2][j] == 0:
+            self.cuadricula[aux2][j] = self.cuadricula[aux1][j]
+            self.cuadricula[aux1][j] = 0
+            aux1 -= 1
+            aux2 -= 1 
+
+    def desplazar_casillas_down(self,i,j):
+            aux1 = i
+            aux2 = i +1
+            while aux2 < FILAS and self.cuadricula[aux2][j] == 0:
+                self.cuadricula[aux2][j] = self.cuadricula[aux1][j]
+                self.cuadricula[aux1][j] = 0
+                aux1 += 1
+                aux2 += 1 
+
     def realizar_accion(self,accion):
-        self._anyadirNumero()
-
-
+        if accion == "right":
+            for i in range(len(self.cuadricula)):
+                for j in reversed(range(len(self.cuadricula[i]))):
+                    if self.cuadricula[i][j] != 0:
+                        self.desplazar_casillas_right(i,j)
+        elif accion == "left":
+            for i in range(len(self.cuadricula)):
+                for j in range(len(self.cuadricula[i])):
+                    if self.cuadricula[i][j] != 0:
+                        self.desplazar_casillas_left(i,j)
+        elif accion == "up":
+            for i in range(len(self.cuadricula)):
+                for j in range(len(self.cuadricula[i])):
+                    if self.cuadricula[i][j] != 0:
+                        self.desplazar_casillas_up(i,j)
+        elif accion == "down":
+            for i in reversed(range(len(self.cuadricula))):
+                for j in reversed(range(len(self.cuadricula[i]))):
+                    if self.cuadricula[i][j] != 0:
+                        self.desplazar_casillas_down(i,j)
+                        
+                        
+        #self._anyadirNumero()
     
     def renderizar(self):
         for i in self.cuadricula:
@@ -47,15 +112,8 @@ class Entorno:
 acciones = {"left":0,"right":1,"up":2,"down":3}
 
 
-
-
-"""
-Para mostrar la matriz:
-    for fila in estados:
-        for valor in fila:
-            print("\t", valor, end=" ")
-        print()
-"""
 entorno = Entorno("ejercicio4", acciones)
 entorno.renderizar()
-#entorno.realizar_accion("right")
+entorno.realizar_accion("down")
+print()
+entorno.renderizar()
